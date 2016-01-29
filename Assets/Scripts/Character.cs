7b3b1e24ebf2e4e68a03fbe15c67d3bb450c6047
Mosphore <dorianessamir.w@gmail.com>
2016-01-30 00:25:38 +0100

    commit du projet

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Character : MonoBehaviour
+{
+
+    public float moveSpeed;
+    Rigidbody playerBody;
+
+    // Use this for initialization
+    void Start()
+    {
+        playerBody = GetComponent<Rigidbody>();
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float z = Input.GetAxis("Vertical");
+        float x = Input.GetAxis("Horizontal");
+
+        if (z != 0)
+        {
+            transform.Translate(0, 0, z * moveSpeed * Time.deltaTime);
+        }
+        if (x != 0)
+        {
+            transform.Translate(x * moveSpeed * Time.deltaTime, 0, 0);
+        }
+
+        if( Input.GetButton("Jump"))
+        {
+            // faire un beau jump ici
+        }
+    }
+}
