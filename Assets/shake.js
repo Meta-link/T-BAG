4786c57a7b6785a763fccf8634d915eab94b9101
Meta-link <Meta-link@hotmail.fr>
2016-01-31 03:45:09 +0100

    Fin des niveaux

---
+++ Assets/shake.js
@@ -0,0 +1,18 @@
+﻿#pragma strict
+
+var cam : GameObject;
+
+public var intensity : float = 0.05;
+var particules : GameObject;
+var particules2 : GameObject;
+
+function Start () {
+particules.SetActive(false);
+particules2.SetActive(false);
+}
+
+function ShakeCam () {
+cam.SendMessage ("DoShake",intensity,SendMessageOptions.DontRequireReceiver);
+particules.SetActive(true);
+particules2.SetActive(true);
+}
